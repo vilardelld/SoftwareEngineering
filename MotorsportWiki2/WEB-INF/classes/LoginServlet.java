3fb2d5d2bee5573b3101beb6f71d316b315380c1
unknown <A904789@B33.alumni.tecnun.edu.es>
2016-04-05 19:02:09 +0200

    april 5 sams first commmit

--- MotorsportWiki2/WEB-INF/classes/LoginServlet.java
+++ MotorsportWiki2/WEB-INF/classes/LoginServlet.java
@@ -1,27 +1,53 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
  
 public class LoginServlet extends HttpServlet{
+	
+	Connection con;
+	public void init(){
+		try{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			System.out.println("creating connection....");
+			con = DriverManager.getConnection("jdbc:odbc:TMSwiki");
+			System.out.println("connection created");
+		}catch (Exception e){
+			System.out.println("db connection failed");
+		}
+		
+	}
+	
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         String name=request.getParameter("username");
         String pass=request.getParameter("password");
-        
+		
+			
+        try{
         if(validate(name,pass)){
-            
+			
+			//create a new HTTPSession
+			HttpSession session = request.getSession();
+			session.setAttribute("user", name);
+			
+			//log out after 30 min inactive
+			sess.ion.setMaxInactiveInterval(30*60);
+
             //Log user into the system. Redirect to main page.
             response.setContentType("text");
-            PrintWriter out = response.getWriter();
-            
+			
+			PrintWriter out = response.getWriter();
             out.print("succeeded");
             out.flush();
             out.close();
+
             
         }else{
             //login failed
-            
+
             response.setContentType("text");
+
             PrintWriter out = response.getWriter();
             
             out.print("failed");
@@ -29,18 +55,60 @@ public class LoginServlet extends HttpServlet{
             out.close();
 
         }
+		}catch(Exception e){
+			System.out.println("db validation failed");
+			System.out.println(e);
+			
+            response.setContentType("text");
+            PrintWriter out = response.getWriter();
+            out.print("failed");
+            out.flush();
+            out.close();
+		}
         
         
     }
     
-    public static boolean validate(String name, String pass){
+    public boolean validate(String name, String pass) throws Exception{
         
         boolean status=false;
         
         //logic to reference database and check username
-        if(name!=null && name!="" && pass!=null && pass!=""){
-            status=true;
-        }
+        System.out.println("checking user "+name);   
+		String query = "SELECT * FROM Usuarios WHERE ID="+name+";";
+
+		Statement stmt = con.createStatement();
+		
+		System.out.println("executing Statement");
+		ResultSet rs = stmt.executeQuery(query);
+		System.out.println("statement executed");
+				
+		while (rs.next()) {
+	      //System.out.println("getting results");
+		  //String dn = rs.getString("Nombre");
+		  //System.out.println("name"+dn);
+		  String dname = rs.getString("ID");
+		  String dpass = rs.getString("Contrasena");
+		  
+		  //System.out.println("From DB: User:"+dname+" Password:"+dpass);
+		  //System.out.println("compare :"+pass+":"+dpass);
+		  
+		  if (pass.equals(dpass)){
+			  System.out.println("Correct credentials");
+			  status=true;
+		  }else{
+			  System.out.println("incorrect credentials");
+			  //status=false;
+		  }
+		}
         return status;
     }
+	
+	public void destroy(){
+		try{
+			con.close();
+		}catch (Exception e){
+			System.out.println("con . close failed");
+		}
+	}
 }
--- MotorsportWiki2/WEB-INF/classes/UserImgServlet.java
+++ MotorsportWiki2/WEB-INF/classes/UserImgServlet.java
@@ -1,78 +1,74 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 public class UserImgServlet extends HttpServlet{
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Connection con;
+			
+	public void init(){
+		try{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			System.out.println("creating connection....");
+			con = DriverManager.getConnection("jdbc:odbc:TMSwiki");
+			System.out.println("connection created");
+		}catch (Exception e){
+			System.out.println("db connection failed");
+		}
+		
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
+		// size of byte buffer to send file
+		
         String name=request.getParameter("username");
-        response.setContentType("text");
-        PrintWriter out = response.getWriter();
-        
-        if(name.equalsIgnoreCase("sam")){
-            
-            //Return img of Sam
-            //response.setContentType("image/jpg");
-            //File img = new File("sam.jpg");
-            //writeImg(img, response);
-            out.write("sam.jpg");
-            
-        }else if (name.equalsIgnoreCase("bernie")){
-            
-            //Return img of Bernie Sanders
-            //response.setContentType("image/jpg");
-            //File img = new File("/bernie.jpeg");
-            //writeImg(img, response);
-            out.write("bernie.jpeg");
-            
-        } else{
-            //no image
-            
-            out.print("no image");
-            out.flush();
-            out.close();
-            
-        }
+
+         
+        try {
+           
+            // queries the database
+			String query = "SELECT * FROM Usuarios WHERE ID = "+name;
+			Statement stmt = con.createStatement();
+			System.out.println("created query");
+			ResultSet rs = stmt.executeQuery(query);
+			System.out.println("executed query");
+
+			response.setContentType("text");
+			PrintWriter out = response.getWriter();
+ 
+            if (rs.next()) {
+                // gets file name and file blob data
+                String fileURL	= rs.getString("imagenURL");
+				System.out.println("file urllll: "+fileURL);
+				if(fileURL==null){
+					System.out.println("putting default image");
+					fileURL="img/default.jpg";
+					
+				}
+
+				out.print(fileURL);     
+				
+            } else {
+                // no file found
+                out.print("no image");  
+            }
+        } catch (SQLException ex) {
+            //ex.printStackTrace();
+            System.out.println("SQL Error: " + ex.getMessage());
+        } catch (IOException ex) {
+            //ex.printStackTrace();
+            //System.out.println("IO Error: " + ex.getMessage());
+        } 
         
     }
-    
-    public void writeImg(File img,HttpServletResponse response){
-        
-        ServletOutputStream outStream = null;
-        //PrintWriter out = response.getWriter();
-        
-        DataInputStream inStream=null;
-        byte[] arrayOfBytes;
-        try
-        {
-            outStream=response.getOutputStream();
-            inStream = new DataInputStream(new FileInputStream(img));
-            arrayOfBytes = new byte[(int) img.length()];
-
-            inStream.readFully(arrayOfBytes);
-            
-            //String imageDataString = Base64.encodeBase64URLSafeString(imageByteArray);
-            //out.write(ImageDataString);
-            outStream.write(arrayOfBytes);
-            outStream.close();
-            
-
-        }
-        catch (Exception e)
-        {
-            arrayOfBytes = null;
-        }
-        finally
-        {
-            try{
-            inStream.close();
-            }
-            catch (Exception f){
-             //failed to close;
-            }
-        }
-        
-
-    }
+	
+	public void destroy(){
+		try{
+			con.close();
+		}catch (Exception e){
+			System.out.println("con . close failed");
+		}
+	}
     
 }
